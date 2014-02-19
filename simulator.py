f32b9b13682628b8ebef7d5aeb8bacc6b8221e39
Harm De Weirdt <harm.deweirdt@gmail.com>
2014-02-19 09:31:43 +0100

    Simulator now also writes the files that a package would normally deploy to the database

--- simulator.py
+++ simulator.py
@@ -22,56 +22,68 @@ with open('test.json', 'r') as data_file:
 
 filesystem = [line.strip() for line in open('filesystem')]
 
+deployment_db = lite.connect('deployment.db')
+pkgdatata_db = lite.connect('pkgfiles.sqlite')
 
 """
 Initialise the database structure
 """
 def init_database():
-    con = lite.connect('deployment.db')
-    with con:
-        cur = con.cursor()
+    with deployment_db:
+        depl_cur = deployment_db.cursor()
         #Create the tables
-        cur.execute("CREATE TABLE IF NOT EXISTS Agent(Name TEXT, UNIQUE(Name));")
+        depl_cur.execute("CREATE TABLE IF NOT EXISTS Agent(Name TEXT, UNIQUE(Name));")
         #Unique so that we don't enter the same ID twice
-        cur.execute("CREATE TABLE IF NOT EXISTS Resource(Id TEXT, UNIQUE(Id));")
-        cur.execute("CREATE TABLE IF NOT EXISTS Attribute(name TEXT, value TEXT, ResourceId TEXT, UNIQUE(name,value,ResourceId));")
-        cur.execute("CREATE TABLE IF NOT EXISTS Relation(name TEXT, side1ID TEXT, side2ID TEXT);")
-        con.commit()
+        depl_cur.execute("CREATE TABLE IF NOT EXISTS Resource(Id TEXT, UNIQUE(Id));")
+        depl_cur.execute("CREATE TABLE IF NOT EXISTS Attribute(name TEXT, value TEXT, ResourceId TEXT, UNIQUE(name,value,ResourceId));")
+        depl_cur.execute("CREATE TABLE IF NOT EXISTS Relation(name TEXT, side1ID TEXT, side2ID TEXT);")
+        deployment_db.commit()
 
 init_database()
 
 def valid_deployment(resource):
-
     res_type = resources.Id.parse_id(resource['id']).get_entity_type()
     logger.info("Resource: %s\nRes type: %s" % (resource,res_type))
 
-    con = lite.connect('deployment.db')
-
-    with con:
-        cur = con.cursor()
-        cur.execute("SELECT * FROM Resource")
-        rows = cur.fetchall()
+    with deployment_db:
+        depl_cur = deployment_db.cursor()
+        depl_cur.execute("SELECT * FROM Resource")
+        #Alternative: select * from Attribute where name like 'path'
+        rows = depl_cur.fetchall()
         directories = [resources.Id.parse_id(row[0]).get_attribute_value() for row in rows]
 
-        if res_type == "std::File":
-            logger.info("Checking for valid File deployment")
-            logger.info("File path: %s " % resource['path'])
-            parent_folder = os.path.dirname(resource['path'])
-            logger.info("Directories: %s" % directories)
-            if not (parent_folder in filesystem or parent_folder in directories):
-                logger.error("Parent folder doesn't exist! Resource not deployed")
+    if res_type == "std::File":
+        logger.info("Checking for valid File deployment")
+        logger.info("File path: %s " % resource['path'])
+        parent_folder = os.path.dirname(resource['path'])
+        logger.info("Directories: %s" % directories)
+        if not (parent_folder in filesystem or parent_folder in directories):
+            logger.error("Parent folder doesn't exist! File not deployed")
 
-        elif res_type == "std::Service":
-            logger.info("Checking for valid Service deployment")
+    elif res_type == "std::Service":
+        logger.info("Checking for valid Service deployment")
+        srv_name = resources.Id.parse_id(resource['id']).get_attribute_value()
+        with pkgdatata_db:
+            pkg_cur = pkgdatata_db.cursor()
+            pkg_cur.execute("SELECT * FROM pkgdata WHERE name LIKE ?", srv_name)
+            srv_files = pkg_cur.fetchall()
 
-        elif res_type == "std::Package":
-            logger.info("Checking for valid Package deployment")
 
-        elif res_type == "std::Directory":
-            logger.info("Checking for valid Directory deployment")
-            logger.info("dirpath: %s " % resource['path'])
+    elif res_type == "std::Package":
+        logger.info("Checking for valid Package deployment")
+
+    elif res_type == "std::Directory":
+        logger.info("Checking for valid Directory deployment")
+        parent_folder = os.path.dirname(resource['path'])
+        logger.info("Directories: %s" % directories)
+        if not (parent_folder in filesystem or parent_folder in directories):
+            logger.error("Parent folder doesn't exist! Directory not deployed")
 
     return True
+
+#Converts two strings like "/usr/bin" and "test/test2" into a list containing "/usr/bin/test" and /usr/bin/test2"
+def filenames_to_files(prefix, filenames):
+    return [prefix + '/' + suffix for suffix in filenames.split('/')]
 
 """
 Write a resource to the database
@@ -79,18 +91,38 @@ Returns the written resource
 Checks doe hier.
 """
 def write_to_database(resource):
-    con = lite.connect('deployment.db')
-    with con:
-        cur = con.cursor()
+    res_type = resources.Id.parse_id(resource['id']).get_entity_type()
+    res_name = resources.Id.parse_id(resource['id']).get_attribute_value()
+
+    with deployment_db:
+        depl_cur = deployment_db.cursor()
+        #First check if there would be no errors during the deployment of this resource
         if valid_deployment(resource):
-            for attr,val in resource.items():
-                if(attr == 'id'):
-                    #Ignore so that we continue if the same Id is entered again
-                    cur.execute("INSERT OR IGNORE INTO Resource VALUES(?)", (val,))
+            for attr, val in resource.items():
+                if attr == 'id' :
+                    depl_cur.execute("INSERT OR IGNORE INTO Resource VALUES(?)", (val,))
                 else:
-                    #First check if there would be no errors during the deployment of this resource
-                    cur.execute("INSERT OR IGNORE INTO Attribute VALUES(?, ?, ?)", (attr, str(val), id))
+                    depl_cur.execute("INSERT OR IGNORE INTO Attribute VALUES(?, ?, ?)", (attr, str(val), resource['id']))
             logger.info("Resource with id %s  written" % resource['id'])
+
+            #Deploy additional resources found in pkginfo
+            if res_type == "std::Package":
+                with pkgdatata_db:
+                    pkg_cur = pkgdatata_db.cursor()
+                    pkg_cur.execute("SELECT * FROM pkgdata WHERE name LIKE (?)", (res_name,))
+                    pkg_files = []
+                    rows = pkg_cur.fetchall()
+                    pkg_files = []
+                    for row in rows:
+                        pkg_files.append(filenames_to_files(row[2], row[3]))
+                    #flatten
+                    pkg_files = [item for sublist in pkg_files for item in sublist]
+
+                    #The id was already written, now write extra path Attributes
+                    for file in pkg_files:
+                        depl_cur.execute("INSERT OR IGNORE INTO Attribute VALUES(?, ?, ?)", ("path", file, resource['id']))
+
+
         else:
             logger.error("Tried to deploy resource but failed")
 
@@ -108,9 +140,9 @@ for res in range(0, len(parsed_json)):
     agent_list.add(agent_name)
 
     #write into db
-    con = lite.connect('deployment.db')
-    with con:
-        cur = con.cursor()
+    deployment_db = lite.connect('deployment.db')
+    with deployment_db:
+        cur = deployment_db.cursor()
         #Ignore so that we continue if the same Id is entered again
         cur.execute("INSERT OR IGNORE INTO Agent VALUES(?)", (agent_name,))
 
